9e09d861b517c4f7b8c09304cda746079544ef79
Baxtiyorjon-Dev <baxtiyorjon.dev@gmail.com>
2024-01-02 11:36:18 +0500

    Image Data Repository changed

--- dataspinacademy/src/main/java/com/dataspin/dataspinacademy/repository/ImageDataRepository.java
+++ dataspinacademy/src/main/java/com/dataspin/dataspinacademy/repository/ImageDataRepository.java
@@ -4,14 +4,15 @@ import com.dataspin.dataspinacademy.entity.ImageData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface ImageDataRepository extends JpaRepository<ImageData, Long> {
     @Modifying
+    @Transactional
     @Query("DELETE FROM ImageData i WHERE i.id IN (:ids)")
     void deleteByIds(List<Long> ids);
 }
