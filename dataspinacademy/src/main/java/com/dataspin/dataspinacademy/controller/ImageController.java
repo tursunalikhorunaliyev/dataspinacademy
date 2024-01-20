480e5d7edb781fabc0990b153770cb9dfac618b4
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2024-01-20 16:41:09 +0500

    image upload is completed

--- dataspinacademy/src/main/java/com/dataspin/dataspinacademy/controller/ImageController.java
+++ dataspinacademy/src/main/java/com/dataspin/dataspinacademy/controller/ImageController.java
@@ -7,12 +7,11 @@ import com.dataspin.dataspinacademy.security.JWTGenerator;
 import com.dataspin.dataspinacademy.service.ImageDataService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api/image")
@@ -26,5 +25,9 @@ public class ImageController {
     @GetMapping("/all")
     public ResponseEntity<ResponseData> allPhoto(HttpServletRequest request){
        return imageDataService.getAll(request);
+    }
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseData> upload(@RequestParam("photo") MultipartFile photo, HttpServletRequest request) throws IOException {
+        return imageDataService.uploadPicture(photo, request);
     }
 }
--- dataspinacademy/src/main/java/com/dataspin/dataspinacademy/service/ImageDataService.java
+++ dataspinacademy/src/main/java/com/dataspin/dataspinacademy/service/ImageDataService.java
@@ -2,6 +2,7 @@ package com.dataspin.dataspinacademy.service;
 
 import com.dataspin.dataspinacademy.dto.ResponseData;
 import com.dataspin.dataspinacademy.entity.ImageData;
+import com.dataspin.dataspinacademy.entity.UserData;
 import com.dataspin.dataspinacademy.projection.ImageDataInfo;
 import com.dataspin.dataspinacademy.repository.ImageDataRepository;
 import com.dataspin.dataspinacademy.security.JWTGenerator;
@@ -12,8 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Base64Utils;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -45,6 +48,21 @@ public class ImageDataService {
             List<String> getAllInfo = imageDataRepository.getAllImages().stream().map(e->"http://dataspinacademy.uz/api/image/?id="+e.getId()).toList();
 
             return ResponseEntity.ok(new ResponseData(true, "Images", getAllInfo));
+        }
+        else{
+            return new ResponseEntity<>(new ResponseData(false, "Siz admin emassiz", null), HttpStatus.BAD_REQUEST);
+        }
+    }
+    public ResponseEntity<ResponseData> uploadPicture(MultipartFile photo, HttpServletRequest request) throws IOException {
+        UserData userData = jwtGenerator.getUserFromRequest(request);
+        if(jwtGenerator.isAdmin(userData)){
+            ImageData imageData = new ImageData();
+            imageData.setFilename(photo.getOriginalFilename());
+            imageData.setContent(photo.getBytes());
+            imageData.setUser(userData);
+
+
+            return ResponseEntity.ok(new ResponseData(true, "Images", "Saqlandi"));
         }
         else{
             return new ResponseEntity<>(new ResponseData(false, "Siz admin emassiz", null), HttpStatus.BAD_REQUEST);
